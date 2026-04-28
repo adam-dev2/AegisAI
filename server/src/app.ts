4ec6a043bb52c5fa9d47783c28919f4e378c8a6a
adam-dev2 <shaikadam252@gmail.com>
2026-04-28 04:05:46 -0700

    Compeleted Login Controller

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import authRoutes from './modules/auth/auth.routes.js'
 
 const app = express();
-
+app.use(express.json())
 app.use('/api/v1/auth',authRoutes);
 
 export default app;
--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,12 +1,48 @@
 import type { Request, Response } from "express";
 import { catchAsync } from "../../lib/catchAsync.js";
 import { AppError } from "../../lib/AppError.js";
+import pool from '../../config/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET, NODE_ENV } from "../../config/env.js";
+
+interface ICookieOptions {
+    httpOnly:boolean,
+    secure:boolean,
+    sameSite:boolean | "lax" | "strict" | "none" | undefined,
+    maxAge:number
+}
 
 export const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { email, password } = req.body;
-
   if (!email || !password) {
     throw new AppError("Email and password required", 400);
   }
-  res.status(200).json({ success: true, data: {} });
+  const result = await pool.query(
+    'SELECT id, email, password_hash FROM users WHERE email = $1',
+    [email]
+  );
+  if (result.rowCount === 0) {
+    throw new AppError("Invalid credentials", 401);
+  }
+  const user = result.rows[0];
+  const isMatch = await bcrypt.compare(password, user.password_hash);
+  if (!isMatch) {
+    throw new AppError("Invalid credentials", 401);
+  }
+  
+  const generateToken = jwt.sign({ id: user.id, email: user.email },JWT_SECRET!,{
+    expiresIn: '1h'
+  })
+  const CookieOptions:ICookieOptions = {
+    httpOnly:true,
+    secure:NODE_ENV === 'production',
+    sameSite:'lax',
+    maxAge:60*100*100
+  }
+  res.cookie('token',generateToken,CookieOptions)
+  res.status(200).json({
+    success: true,
+    data: { id: user.id, email: user.email }
+  });
 });
