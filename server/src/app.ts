64b7c07a2222e3fe10a4e6083cd7ff44d6b56799
adam-dev2 <shaikadam252@gmail.com>
2026-04-29 05:27:08 -0700

    completed polling need to work on tools

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import authRoutes from './modules/auth/auth.routes.js'
+import siemRoutes from './modules/siem/siem.routes.js'
+import alertsRoutes from './modules/alerts/alerts.routes.js'
+import investigationRoutes from './modules/investigation/investigation.routes.js'
 
 const app = express();
 app.use(express.json())
@@ -10,5 +13,8 @@ app.get('/',(req,res) => {
     })
 })
 app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/siem',siemRoutes);
+app.use('/api/v1/investigation',investigationRoutes);
+app.use('/api/v1/alerts',alertsRoutes)
 
 export default app;
---
+++ server/src/modules/investigation/investigation.routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { mfaVerifiedMiddleware } from '../../middleware/auth.middleware.js';
+import { pollingStatus, startPolliing, stopPolliing, updatePollingInterval } from './investigation.controllers.js';
+
+const router = express.Router();
+
+// router.use(mfaVerifiedMiddleware)
+
+router.post('/polling/start',startPolliing)
+router.post('/polling/stop',stopPolliing)
+router.get('/polling/status',pollingStatus)
+router.patch('/polling/interval',updatePollingInterval)
+// router.post('/:alertId/status')
+
+export default router;
